d32ebe683dd7c43accc9c330ba0e459a2bc67ddf
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2023-12-02 22:48:13 +0530

    🦋Opfile2Link-BOT

---
+++ script.sh
@@ -0,0 +1,1 @@
+apt-get install python3 -y && apt-get install virtualenv -y && apt-get install git -y && git clone https://github.com/SudoR2spr/Opfile2Link-BOT && cd Opfile2Link-BOT && virtualenv -p /usr/bin/python3 venv && . ./venv/bin/activate && pip install -r requirements.txt && python3 cli.py && python3 -m Adarsh
